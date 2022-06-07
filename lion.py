e62006e77e71f7773209c816b80ffb206ab2506a
ansk1d <105351282+ansk1d@users.noreply.github.com>
2022-06-07 23:29:46 +0300

    finally!

---
+++ lion.py
@@ -0,0 +1,21 @@
+class Lion:
+    def __init__(self,name,health_level = 6 ,happiness_level = 7, age = "unknown"):
+        self.name = name
+        self.age = age
+        self.health = health_level
+        self.happiness = happiness_level
+    
+    def display_info(self):
+        print(
+            f"Name: {self.name}",
+            f"Type: {type(self).__name__}",
+            f"Age: {self.age}",
+            f"Heath: {self.health}",
+            f"Happiness: {self.happiness}",
+            sep="\n" , end="\n\n")
+        return self
+    
+    def feed(self):
+        self.health += 10
+        self.happiness += 10
+        return self
